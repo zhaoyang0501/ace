d66d9fa864fee19e7a9c29cc1a1bdab93a6b9116
zhaoyang0501 <zhaoyang0501@126.com>
2015-04-21 17:41:37 +0800

    : New Task
    Task-Url:

---
+++ src/main/webapp/admin/js/ace/admin.user.js
@@ -0,0 +1,108 @@
+jQuery.adminUser = {
+		userDataTable:null,
+		initSearchDataTable : function() {
+			if (this.userDataTable == null) {
+				this.userDataTable = $('#dt_user_view').dataTable({
+					"sDom" : "<'row-fluid'<'span6'l>r>t<'row-fluid'<'span6'i><'span6'p>>",
+					"sPaginationType" : "bootstrap",
+					"oLanguage" : {
+						"sLengthMenu" : "每页显示 _MENU_ 条记录",
+						"sZeroRecords" : "抱歉， 暂时没有记录",
+						"sInfo" : "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+						"sSearch" : "",
+						"sInfoEmpty" : "没有数据",
+						"sInfoFiltered" : "(从 _MAX_ 条数据中检索)",
+						"oPaginate" : {
+							"sFirst" : "首页",
+							"sPrevious" : "前一页",
+							"sNext" : "后一页",
+							"sLast" : "尾页"
+						}
+					},
+					"bAutoWidth" : false,
+					"iDisplayLength" : 10,
+					"aLengthMenu" : [ 5, 10, 25, 50],
+					"bServerSide" : true,
+					"sServerMethod" : "POST",
+					"bProcessing" : true,
+					"bSort" : false,
+					"sAjaxSource" : $.ace.getContextPath() + "/admin/user/list",
+					"fnDrawCallback" : function(oSettings) {
+						$('[rel="popover"],[data-rel="popover"]').popover();
+					},
+					"fnServerData" : function(sSource, aoData, fnCallback) {
+						var userName = $("#userName").val();
+						if (!!userName) {
+							aoData.push({
+								"name" : "userName",
+								"value" : userName
+							});
+						}
+						$.ajax({
+							"dataType" : 'json',
+							"type" : "POST",
+							"url" : sSource,
+							"data" : aoData,
+							"success" : function(data){
+								fnCallback(data.resultMap);
+							}
+						});
+					},
+					"aoColumns" : [ {
+						"mDataProp" : "id"
+					}, {
+						"mDataProp" : "userName"
+					}, {
+						"mDataProp" : "password"
+					}, {
+						"mDataProp" : "school"
+					}, {
+						"mDataProp" : "className"
+					}, {
+						"mDataProp" : "sex"
+					}, {
+						"mDataProp" : "birtthDay"
+					},{
+						"mDataProp" : ""
+					}],
+					"aoColumnDefs" : [
+						{
+							'aTargets' : [7],
+							'fnRender' : function(oObj, sVal) {
+								return "<a class=\"btn2 btn-info\" onclick=\"$.adminUser.deleteUser("+oObj.aData.id+")\"><i class=\"icon-zoom-in\"></i> 删除</a>";
+							}
+						},
+					 {
+						'aTargets' : [ '_all' ],
+						'bSortable' : false,
+						'sClass' : 'center'
+					}]
+
+				});
+			} else {
+				var oSettings = this.userDataTable.fnSettings();
+				oSettings._iDisplayStart = 0;
+				this.userDataTable.fnDraw(oSettings);
+			}
+
+		},
+		deleteUser :function(id){
+			bootbox.confirm( "是否确认删除？", function (result) {
+	            if(result){
+	            	$.ajax({
+	        			type : "get",
+	        			url : $.ace.getContextPath() + "/admin/user/list/delete?id="+id,
+	        			dataType : "json",
+	        			success : function(json) {
+	        				if(json.resultMap.state=='success'){
+	        					noty({"text":""+ json.resultMap.msg +"","layout":"top","type":"success","timeout":"2000"});
+	        					$.ace.adminUser.initSearchDataTable();
+	        				}else{
+	        					noty({"text":""+ json.resultMap.msg +"","layout":"top","type":"warning"});
+	        				}
+	        			}
+	        		});
+	            }
+	        });
+		}
+};
